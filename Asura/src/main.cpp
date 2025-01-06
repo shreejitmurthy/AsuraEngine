eacb4b3b2ac4cf82692034478b9a177487a762bc
Shreejit Murthy <botmorph17@gmail.com>
2025-01-06 12:39:58 +0530

    create window and clear screen

--- Asura/src/main.cpp
+++ Asura/src/main.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
 
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#include <glad/glad.h>
+
+#include <spdlog/spdlog.h>
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        spdlog::error("ASURA::Initialise Error: {}", SDL_GetError());
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    SDL_Window* window = SDL_CreateWindow("Window", 800, 600, SDL_WINDOW_OPENGL);
+    SDL_GLContext ctx = SDL_GL_CreateContext(window);
+    bool open = true;
+
+    if (gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress) == 0) {
+        spdlog::error("ASURA::Failed to initialise GLAD");
+    }
+
+    SDL_GL_SetSwapInterval(1);
+
+    SDL_Event event;
+
+    while (open) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_EVENT_QUIT:
+                    open = false;
+                    break;
+
+                case SDL_EVENT_KEY_DOWN:
+                    if (event.key.key == SDLK_ESCAPE) {
+                        open = false;
+                    }
+
+                default:
+                    break;
+            }
+        }
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_GL_DestroyContext(ctx);
+    spdlog::info("ASURA::Destroyed Window and GL Context");
+    SDL_Quit();
+    spdlog::info("ASURA::Goodbye!");
+
     return 0;
 }
