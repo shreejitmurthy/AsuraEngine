ac84160fb2d30b5599bed199a11775bf55dcdc9f
Shreejit Murthy <botmorph17@gmail.com>
2025-01-11 21:36:03 +0530

    define an entry point

---
+++ Asura/include/Asura/Entry.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef AS_PLATFORM_MACOS
+
+extern Asura::Application* Asura::CreateApplication();
+
+int main(int argc, char* argv[]) {
+    auto app = Asura::CreateApplication();
+    app->Run();
+    delete app;
+}
+
+#endif
