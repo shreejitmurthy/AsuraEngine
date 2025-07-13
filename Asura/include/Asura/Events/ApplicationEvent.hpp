e9da5fff95ab24b43d0c7318d5cac91a8510d048
Shreejit Murthy <botmorph17@gmail.com>
2025-07-13 16:42:53 +1000

    application events

---
+++ Asura/include/Asura/Events/ApplicationEvent.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Event.hpp"
+#include "Core.hpp"
+
+#include <sstream>
+
+namespace Asura {
+    class ASURA_API WindowCloseEvent : Event {
+    public:
+        WindowCloseEvent() {}
+        EVENT_CLASS_TYPE(WindowClose)
+        EVENT_CLASS_CATEGORY(EventCategoryApplication)
+    };
+
+    class ASURA_API WindowResizeEvent : Event {
+    public:
+        WindowResizeEvent(unsigned int w, unsigned int h) : _width(w), _height(h) {}
+
+        inline unsigned int GetWidth()  const { return _width; };
+        inline unsigned int GetHeight() const { return _height; };
+
+        std::string ToString() const override {
+            std::stringstream ss;
+            ss << "WindowResizeEvent: " << _width << ", " << _height;
+            return ss.str();
+        }
+        
+        EVENT_CLASS_TYPE(WindowResize)
+        EVENT_CLASS_CATEGORY(EventCategoryApplication | EventCategoryWindow)
+    private:
+        unsigned int _width, _height;
+    };
+
+
+}
