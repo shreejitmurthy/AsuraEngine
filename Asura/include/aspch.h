f365bc4a88fcfb290cd4e941717268d6c3294423
Shreejit Murthy <botmorph17@gmail.com>
2025-07-17 21:38:52 +1000

    precompiled header

---
+++ Asura/include/aspch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <cstdint>
+#include <sstream>
+#include <ostream>
+#include <queue>
+#include <functional>
+#include <vector>
+
+#ifdef AS_PLATFORM_MACOS
+#endif
+
+#ifdef AS_PLATFORM_WIN64
+    #include <Windows.h>
+#endif
